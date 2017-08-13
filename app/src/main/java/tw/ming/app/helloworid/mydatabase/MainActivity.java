0f6aeb7bc675a0d5a7d01dfcb9906d205190ded4
LBJ <LBJ>
2017-08-13 14:47:21 +0800

    20170813

--- app/src/main/java/tw/ming/app/helloworid/mydatabase/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/mydatabase/MainActivity.java
@@ -1,13 +1,43 @@
 package tw.ming.app.helloworid.mydatabase;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MyDBHelper myDBHelper;
+    private SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDBHelper = new MyDBHelper(this,"ming",null,1);
+        db = myDBHelper.getReadableDatabase();
+
+    }
+    public void insert(View view){
+
+    }
+    public void delete(View view){
+
+    }
+    public void update(View view){
+
+    }
+    public void query(View view){
+        //select * from cust
+        Cursor cursor= db.query("cust",null,null,null,null,null,null);
+        while(cursor.moveToNext()){
+            String id = cursor.getString(0);
+            String cname = cursor.getString(1);
+            String tel = cursor.getString(2);
+            String birthday = cursor.getString(3);
+            Log.i("ming",id+":"+cname+":"+tel+":"+birthday);
+
+        }
     }
 }
