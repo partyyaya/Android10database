762b5ff407e4437376d0870737837e3a377c7847
LBJ <LBJ>
2017-08-13 15:28:58 +0800

    20170813

--- app/src/main/java/tw/ming/app/helloworid/mydatabase/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/mydatabase/MainActivity.java
@@ -1,43 +1,64 @@
 package tw.ming.app.helloworid.mydatabase;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private MyDBHelper myDBHelper;
     private SQLiteDatabase db;
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myDBHelper = new MyDBHelper(this,"ming",null,1);
+        tv = (TextView)findViewById(R.id.tv);
+
+        myDBHelper = new MyDBHelper(this, "ming", null, 1);
         db = myDBHelper.getReadableDatabase();
 
+        query(null);
     }
-    public void insert(View view){
 
+    public void insert(View view){
+        // insert into cust (cname,tel,birthday) values ('xxx','xxx','xxx');
+        ContentValues values = new ContentValues();
+        values.put("cname", "ming");
+        values.put("tel", "123");
+        values.put("birthday", "1999-01-02");
+        db.insert("cust", null, values);
+        query(null);
     }
     public void delete(View view){
-
+        // delete from cust where _id = 3 and cname='Brad'
+        db.delete("cust","_id=? and cname=?", new String[]{"3","ming"});
+        query(null);
     }
     public void update(View view){
-
+        ContentValues values = new ContentValues();
+        values.put("cname", "jacky");
+        values.put("tel", "12313");
+        values.put("birthday", "1199-01-02");
+        db.update("cust",values,"_id=?",new String[]{"5"});
+        query(null);
     }
     public void query(View view){
-        //select * from cust
-        Cursor cursor= db.query("cust",null,null,null,null,null,null);
-        while(cursor.moveToNext()){
-            String id = cursor.getString(0);
-            String cname = cursor.getString(1);
-            String tel = cursor.getString(2);
-            String birthday = cursor.getString(3);
-            Log.i("ming",id+":"+cname+":"+tel+":"+birthday);
-
+        // select * from cust
+        tv.setText("");
+        Cursor cursor = db.query("cust",null,null,null,null,null,null);
+        while (cursor.moveToNext()){
+            String id = cursor.getString(cursor.getColumnIndex("_id"));
+            String cname = cursor.getString(cursor.getColumnIndex("cname"));
+            String tel = cursor.getString(cursor.getColumnIndex("tel"));
+            String birthday = cursor.getString(cursor.getColumnIndex("birthday"));
+            tv.append(id+":"+cname+":"+tel+":"+birthday + "\n");
         }
     }
 }
